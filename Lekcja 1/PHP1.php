092f010163c363d4bcd74e26ee7900433fda9e3b
BSpulikowski3B <borysspulikowski@wp.pl>
2022-09-13 12:40:58 +0200

    Lekcja1

---
+++ Lekcja 1/PHP1.php
@@ -0,0 +1,23 @@
+<style>
+P{
+	color: blue;
+}
+	</style>
+<html lang="pl">
+	<head> 
+		<link rel="stylesheet" href="">
+		<meta charset="UTF-8">
+		<meta name="keywords" content="HTML, CSS, JavaScript, PHP">
+
+	</head>
+	<body>
+        <?PHP
+        $z = 1;
+        echo "<p>HEllO WORLD <br>
+        My counter $z <br>
+        HEllO Friend</p>"
+
+        ?>
+        
+	</body>
+</html>
